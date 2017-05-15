0165516b1a25c5cb7e37f1e45038d89d03f1e088
redlion-entity <redlion.2016@ya.ru>
2017-05-15 06:24:51 +0300

    some corrections for the discnt

--- discnt/discnt.cpp
+++ discnt/discnt.cpp
@@ -40,85 +40,89 @@ void insert(int *p, int index, int **d_prices, int *d_p_index, int quantity) {
   }
   
   if (quantity - *d_p_index > 1) {
-    *d_p_index++;
+      (*d_p_index)++;
   }
 }
 
 int main(int argc, char *argv[]) {
 	char *input_file, *output_file;
 	FILE *fp = NULL;
-  int prices[10001], quantity = 0, price, discount;
-  int discount_quantity, discount_prices_index = -1;
-  int **discount_prices;
-  int prices_sum = 0, discount_prices_sum = 0;
-  float sum;
-  int i, insert_index;
+    int prices[10001], quantity = 0, price, discount;
+    int discount_quantity, discount_prices_index = -1;
+    int **discount_prices;
+    int prices_sum = 0, discount_prices_sum = 0;
+    float sum;
+    int i, insert_index;
 
-	if (argc >= 2) {
-		input_file = argv[1];	
-	} else {
-		input_file = "discnt.in";
-	}
-  
-  if (argc >= 3) {
-		output_file = argv[2];	
-	} else {
-		output_file = "discnt.out";
-	}
-  
-  fp = fopen(input_file, "r");
-  if (fp == NULL) {
-    return EXIT_FAILURE;
-  }
-  while(fscanf(fp, "%d", &price) == 1) {
-    prices[quantity++] = price;
-  }
-  discount = prices[--quantity];
-  fclose(fp);
-  
-  discount_quantity = quantity / 3;
-  if (discount_quantity > 0) {
-    discount_prices = new int*[discount_quantity];
-  }
-  
-  for (i = 0; i < quantity; i++) {
-    prices_sum += prices[i];
-    
-    if (discount_quantity > 0) {
-      if (discount_prices_index >= 0) {
-        if (prices[i] > *discount_prices[0]) {
-          insert_index = 0;
-        } else if (prices[i] == *discount_prices[0]) {
-          insert_index = 1;
-        } else if (prices[i] <= *discount_prices[discount_prices_index]) {
-          insert_index = discount_prices_index + 1;
-        } else {
-          insert_index = search_index(prices[i], discount_prices, discount_prices_index);
-        }
-        
-        if (insert_index < discount_quantity) {
-          insert(&prices[i], insert_index, discount_prices, &discount_prices_index, discount_quantity);
-        }
-      } else {
-        discount_prices[++discount_prices_index] = &prices[i];
-      }
+    if (argc >= 2) {
+        input_file = argv[1];
+    } else {
+        //input_file = "/Users/red_lion/Documents/Projects/GitHub/algorithms-lits/discnt/inputData/08.in";
+        input_file = "discnt.in";
     }
-  }
-  
-  for (i = 0; i < discount_quantity; i++) {
-    discount_prices_sum += *discount_prices[i];
-  }
-  
-  delete [] discount_prices;
-  
-  sum = prices_sum - discount_prices_sum * discount / 100.0;
-  
-  fp = fopen(output_file, "w");
-  if (fp == NULL) {
-    return EXIT_FAILURE;
-  }
-  fprintf(fp, "%.2f", sum);
-  fclose(fp);
-  
-  return 0;
+
+    if (argc >= 3) {
+        output_file = argv[2];
+    } else {
+        //output_file = "/Users/red_lion/Documents/Projects/GitHub/algorithms-lits/discnt/discnt.out";
+        output_file = "discnt.out";
+    }
+
+    fp = fopen(input_file, "r");
+    if (fp == NULL) {
+        return EXIT_FAILURE;
+    }
+    while(fscanf(fp, "%d", &price) == 1) {
+        prices[quantity++] = price;
+    }
+    discount = prices[--quantity];
+    fclose(fp);
+
+    discount_quantity = quantity / 3;
+    if (discount_quantity > 0) {
+        discount_prices = new int*[discount_quantity];
+    }
+
+    for (i = 0; i < quantity; i++) {
+        prices_sum += prices[i];
+
+        if (discount_quantity > 0) {
+            if (discount_prices_index >= 0) {
+                if (prices[i] > *discount_prices[0]) {
+                    insert_index = 0;
+                } else if (prices[i] == *discount_prices[0]) {
+                    insert_index = 1;
+                } else if (prices[i] <= *discount_prices[discount_prices_index]) {
+                        insert_index = discount_prices_index + 1;
+                } else {
+                        insert_index = search_index(prices[i], discount_prices, discount_prices_index);
+                }
+
+                if (insert_index < discount_quantity) {
+                    insert(&prices[i], insert_index, discount_prices, &discount_prices_index, discount_quantity);
+                }
+            } else {
+                discount_prices[++discount_prices_index] = &prices[i];
+            }
+        }
+    }
+
+    for (i = 0; i < discount_quantity; i++) {
+        discount_prices_sum += *discount_prices[i];
+    }
+
+    if (*discount_prices != NULL) {
+        delete [] discount_prices;
+    }
+
+    sum = prices_sum - discount_prices_sum * discount / 100.0;
+
+    fp = fopen(output_file, "w");
+    if (fp == NULL) {
+        return EXIT_FAILURE;
+    }
+    fprintf(fp, "%.2f", sum);
+    fclose(fp);
+
+    return 0;
 }
