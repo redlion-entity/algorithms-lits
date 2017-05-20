b9d2aed8aac5b52435ef9354605e29b6b580dc25
redlion-entity <redlion.2016@ya.ru>
2017-05-21 01:05:24 +0300

    LNGPOK: The work of the program is fixed

--- lngpok/lngpok.cpp
+++ lngpok/lngpok.cpp
@@ -20,11 +20,10 @@ int getJokerQuantity(int *A, int length) {
 }
 
 int** uniqueSorted(int *A, int length, int *quantity) {
-    int **temp = new int*[length], **result;
+    int **temp = new int*[length], **result = NULL;
     int i, lastValue = 0;
 
     *quantity = 0;
-    result = NULL;
 
     for (i = 0; i < length; i++) {
         if (A[i] != lastValue) {
@@ -66,16 +65,8 @@ int findSequence(int **A, int *start_index, int *end_index, int length, int *fre
             *free_jokers = *free_jokers - (difference - 1);
             quantity = difference  + quantity;
         } else if (*free_jokers > 0) {
-            temp = *A[*end_index] + *free_jokers;
-
-            if (temp <= 1000000) {
-                quantity += *free_jokers;
-                *free_jokers = 0;
-            } else {
-                temp = 1000000 - *A[*end_index];
-                quantity += temp;
-                *free_jokers = *free_jokers - temp;
-            }
+            quantity += *free_jokers;
+            *free_jokers = 0;
         } else {
             break;
         }
@@ -83,7 +74,20 @@ int findSequence(int **A, int *start_index, int *end_index, int length, int *fre
 
     // if the end is already reached and there are still jokers
     if (i >= length && *free_jokers > 0) {
-        quantity += MIN(*free_jokers, *A[*start_index] - 1);
+        temp = *A[*end_index] + *free_jokers;
+
+        if (temp <= 1000000) {
+            quantity += *free_jokers;
+            *free_jokers = 0;
+        } else {
+            temp = 1000000 - *A[*end_index];
+            quantity += temp;
+            *free_jokers = *free_jokers - temp;
+
+            temp = MIN(*free_jokers, *A[*start_index] - 1);
+            *free_jokers = *free_jokers - temp;
+            quantity += temp;
+        }
     }
 
     return quantity;
@@ -93,7 +97,7 @@ int findNextStartIndex(int **A, int start_index, int length) {
     int i;
 
     for (i = start_index + 1; i < length; i++) {
-        if (A[i] - A[i - 1] > 1) {
+        if (*A[i] - *A[i - 1] > 1) {
             break;
         }
     }
@@ -105,14 +109,14 @@ int main(int argc, char *argv[]) {
     char *input_file, *output_file;
     FILE *fp = NULL;
     int cards[100000], quantity = 0, card;
-    int **sorted_uniqued_cards;
+    int **sorted_uniqued_cards = NULL;
     int i, joker_quantity, free_joker_quantuty, sorted_uniqued_cards_quantity;
     int start_index = 0, end_index = 0, sequence_length = 1, max_sequence_length = 0, next_start_index;
 
     if (argc >= 2) {
         input_file = argv[1];
     } else {
-        //input_file = "/Users/red_lion/Documents/Projects/GitHub/algorithms-lits/lngpok/inputData/04.in";
+        //input_file = "/Users/red_lion/Documents/Projects/GitHub/algorithms-lits/lngpok/inputData/01.in";
         input_file = "lngpok.in";
     }
 
@@ -136,35 +140,45 @@ int main(int argc, char *argv[]) {
     // sort cards
     qSort(cards, quantity);
 
-    // unique cards
     joker_quantity = getJokerQuantity(cards, quantity);
-    sorted_uniqued_cards = uniqueSorted(&cards[joker_quantity], quantity - joker_quantity, &sorted_uniqued_cards_quantity);
 
-    free_joker_quantuty = joker_quantity;
-    while (end_index < sorted_uniqued_cards_quantity - 1) {
-        sequence_length = findSequence(sorted_uniqued_cards, &start_index, &end_index, sorted_uniqued_cards_quantity, &free_joker_quantuty, sequence_length);
-        max_sequence_length = MAX(max_sequence_length, sequence_length);
+    if (quantity - joker_quantity > 0) {
+        // unique cards
+        sorted_uniqued_cards = uniqueSorted(&cards[joker_quantity], quantity - joker_quantity, &sorted_uniqued_cards_quantity);
 
-        // count first index of next sub sequence without jokers
-        next_start_index = findNextStartIndex(sorted_uniqued_cards, start_index, sorted_uniqued_cards_quantity);
-        if (next_start_index > end_index) {
-            end_index = next_start_index;
+        free_joker_quantuty = joker_quantity;
+        while (end_index < sorted_uniqued_cards_quantity) {
+            sequence_length = findSequence(sorted_uniqued_cards, &start_index, &end_index, sorted_uniqued_cards_quantity, &free_joker_quantuty, sequence_length);
+            max_sequence_length = MAX(max_sequence_length, sequence_length);
+
+            // count first index of next sub sequence without jokers
+            next_start_index = findNextStartIndex(sorted_uniqued_cards, start_index, sorted_uniqued_cards_quantity);
+            if (next_start_index > end_index) {
+                end_index = next_start_index;
+            }
+
+            // count free jokers
+            if (joker_quantity > 0 && next_start_index > 0 && next_start_index < sorted_uniqued_cards_quantity) {
+                if (end_index == next_start_index) {
+                    free_joker_quantuty = joker_quantity;
+                } else {
+                    free_joker_quantuty = sequence_length - (*sorted_uniqued_cards[next_start_index - 1] - *sorted_uniqued_cards[start_index] + 1) - (*sorted_uniqued_cards[end_index] - *sorted_uniqued_cards[next_start_index] + 1);
+                }
+            }
+
+            // count next sequence length
+            if (end_index == next_start_index) {
+                sequence_length = 1;
+            } else {
+                sequence_length = *sorted_uniqued_cards[end_index] - *sorted_uniqued_cards[next_start_index] + 1;
+            }
+
+            start_index = next_start_index;
         }
-
-        // count free jokers
-        if (joker_quantity > 0 && next_start_index > 0 && next_start_index < sorted_uniqued_cards_quantity) {
-            free_joker_quantuty = MIN(*sorted_uniqued_cards[next_start_index] - *sorted_uniqued_cards[next_start_index - 1] - 1 + free_joker_quantuty, joker_quantity);
-        }
-
-        // count next sequence length
-        if (end_index == next_start_index) {
-            sequence_length = 0;
-        } else {
-            sequence_length = next_start_index - start_index + (*sorted_uniqued_cards[next_start_index] - *sorted_uniqued_cards[next_start_index - 1] - 1);
-        }
-
-        start_index = next_start_index;
+    } else {
+        max_sequence_length = MIN(joker_quantity, 10000);
     }
+
 
 
     /*printf("Sorted cards: ");
@@ -184,7 +198,9 @@ int main(int argc, char *argv[]) {
 
 
 
-    delete [] sorted_uniqued_cards;
+    if (sorted_uniqued_cards != NULL) {
+        delete [] sorted_uniqued_cards;
+    }
 
     fp = fopen(output_file, "w");
     if (fp == NULL) {
